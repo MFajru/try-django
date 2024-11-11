08bed2037e019909ca9e4bec3eedd4691f2e4755
MFajru <fajru234@gmail.com>
2024-11-11 12:26:01 +0700

    part 3: django tutorial

--- apigraphql/settings.py
+++ apigraphql/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,17 @@ WSGI_APPLICATION = 'apigraphql.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'try-django',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        "OPTIONS": {
+            # "service": "postgres_service",
+            # "passfile": ".postgres_pass"
+        },
+        
     }
 }
 
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+
+from polls.models import Question
 
 def index(request):
-    return HttpResponse("hello world, you are in polls view.")
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+    # return HttpResponse("hello world, you are in polls view.")
+
+def detail(request, question_id):
+    # question = get_object_or_404(Question, pk = question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
 
